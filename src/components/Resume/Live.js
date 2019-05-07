28cc460fe855a278dfa677ecf8dd049f6afef85c
celestinosalim <celestino.salim@gmail.com>
2019-05-07 13:54:10 -0400

    modified live component, only takes care of a scrolling to top after redirecting

--- src/components/Resume/Live.js
+++ src/components/Resume/Live.js
@@ -1,22 +1,4 @@
-// REACT
-//------------------------------------------------------------------------------------------
 import React from "react";
-import { connect } from "react-redux";
-
-// TEMPLATE
-//------------------------------------------------------------------------------------------
-
-// import Header from "./PreviewSections/Header";
-// import ProfileSection from "./PreviewSections/ProfileSection";
-// import EducationSection from "./PreviewSections/EducationSection";
-// import ExperienceSection from "./PreviewSections/ExperienceSection";
-// import SkillSection from "./PreviewSections/SkillSection";
-// import ProjectSection from "./PreviewSections/ProjectSection";
-// import FooterSection from "./PreviewSections/FooterSection";
-// import SideNavPage from "./SideNavPage";
-// STYLE
-//------------------------------------------------------------------------------------------
-import "../../style/preview.css";
 
 const timing = () => {
   setTimeout(() => {
@@ -33,8 +15,4 @@ const Live = props => {
   );
 };
 
-const mapStateToProps = state => {
-  return { resume: state.resume.resume };
-};
-
-export default connect(mapStateToProps)(Live);
+export default Live;

c0593bef14e1eb4bae5fafffb66d55b7e417bc6e
celestinosalim <celestino.salim@gmail.com>
2019-05-07 18:14:51 -0400

    changed preview.css, preview.js, template1 and template2, added a new carousel, fixed some css, starting to add properties to template2 to later fix css on whole application

--- src/components/Resume/Template2/Resume2.js
+++ src/components/Resume/Template2/Resume2.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
+import {
+  SideBar,
+  Intro,
+  About,
+  ProjectContainer,
+  Skills,
+  Footer
+} from "vs-template1";
+
 class Resume2 extends Component {
   state = {};
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Intro />
+        <SideBar />
+        <div>
+          <About />
+        </div>
+        <Skills />
+        <ProjectContainer />
+        <Footer />
       </div>
     );
   }
---
+++ src/store/actions/TemplateActions.js
@@ -0,0 +1,32 @@
+import * as types from "./ActionTypes";
+
+const setTemplate = resume => {
+  return {
+    type: types.UPDATE_TEMPLATE,
+    resume
+  };
+};
+
+export const updateTemplate = (resumeId, template) => {
+  return dispatch => {
+    return fetch(`${types.BASE_URL}/api/resumes/${resumeId}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`,
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({
+        resume: {
+          template: template
+        }
+      })
+    })
+      .then(response => response.json())
+      .then(resume => {
+        dispatch(setTemplate(resume));
+      })
+
+      .catch(error => console.log(error));
+  };
+};

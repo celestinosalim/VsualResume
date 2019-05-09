8ea84bc0a7c4387438069423be0c3ecb4eba987c
celestinosalim <celestino.salim@gmail.com>
2019-05-08 21:14:54 -0400

    changed template2

--- src/components/Resume/Template2/Template2.js
+++ src/components/Resume/Template2/Template2.js
@@ -12,6 +12,51 @@ import {
   Footer
 } from "vs-template1";
 
+const sidebar = {
+  navLinks: [
+    {
+      pathName: "About",
+      pathRef: "about"
+    },
+    {
+      pathName: "Skills",
+      pathRef: "skills"
+    },
+    {
+      pathName: "Projects",
+      pathRef: "projects"
+    },
+    {
+      pathName: "Contact",
+      pathRef: "footer"
+    }
+  ]
+};
+
+const skillsToRender = skill => {
+  return skill.map(obj => obj.name);
+};
+
+const ProjectstoRender = projects => {
+  return projects.map(project => {
+    return {
+      imageURL: project.image,
+      title: project.name,
+      link: project.url,
+      languages: project.description.split(",")
+    };
+  });
+};
+
+const mediaToRender = media => {
+  return media.map(media => {
+    return {
+      name: media.name,
+      link: media.link
+    };
+  });
+};
+
 class Template2 extends Component {
   setTemplate = e => {
     let templateId = 2;
@@ -24,6 +69,7 @@ class Template2 extends Component {
   };
 
   render() {
+    let { resume } = this.props;
     return (
       <div>
         <Link
@@ -33,12 +79,46 @@ class Template2 extends Component {
         >
           SELECT TEMPLATE
         </Link>
-        <Intro />
-        {this.props.location.pathname === "/resume/review" ? null : <SideBar />}
-        <About />
-        <Skills />
-        <ProjectContainer />
-        <Footer />
+
+        {/* {this.props.location.pathname === "/resume/review" ? null : <SideBar />} */}
+        {resume && (
+          <div>
+            <div id="intro">
+              <Intro
+                name={resume.profile.name}
+                profession={resume.profile.headline}
+                link={"about"}
+              />
+            </div>
+
+            <SideBar
+              name="Menu"
+              navLinks={sidebar.navLinks}
+              color="#00aced"
+              linkColor="#00aced"
+            />
+
+            <div id="about">
+              <About
+                name={resume.profile.name}
+                imageURL={resume.profile.profile_picture}
+                description={resume.profile.about_me}
+              />
+            </div>
+            <div id="skills">
+              <Skills skills={[...skillsToRender(resume.tools)]} />
+            </div>
+            <div id="projects">
+              <ProjectContainer projects={ProjectstoRender(resume.projects)} />
+            </div>
+            <div id="footer">
+              <Footer
+                name={resume.profile.name}
+                media={mediaToRender(resume.social_medium)}
+              />
+            </div>
+          </div>
+        )}
       </div>
     );
   }
